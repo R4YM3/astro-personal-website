f803937dd56c3663a94b9dd2b3245a3ea4ac6128
Raymond Schweers <raymond@hetwebbureau.nl>
2022-10-12 21:50:15 +0200

    feat(astro) set base url to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.raymondschweers.nl',
+  base: '/',
   integrations: [tailwind()]
 });
