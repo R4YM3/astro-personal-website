156102497ba88ffdb092084caf6a37d046f230cc
Raymond Schweers <raymond@hetwebbureau.nl>
2022-10-16 21:35:44 +0200

    feat(Employees) init

---
+++ src/constants/employees.ts
@@ -0,0 +1,148 @@
+import alphaplusLogo from "../images/clients/alphaplus.png";
+import brandshakersLogo from "../images/clients/brandshakers.svg";
+import cosmopolitanLogo from "../images/clients/cosmopolitan.svg";
+import dhlLogo from "../images/clients/dhl.svg";
+import ebayLogo from "../images/clients/ebay.svg";
+import ebuddyLogo from "../images/clients/ebuddy.svg";
+import flyershotsLogo from "../images/clients/flyershots.png";
+import geekSquadLogo from "../images/clients/geek-squad.svg";
+import gemeenteHilversum from "../images/clients/gemeente-hilversum.svg";
+import hansTeeuwen from "../images/clients/hansTeeuwen.jpg";
+import klantenBedrijfLogo from "../images/clients/klantenbedrijf.png";
+import kvkLogo from "../images/clients/kvk.svg";
+import mediamarktLogo from "../images/clients/mediamarkt.svg";
+import middelmanWonenLogo from "../images/clients/middelman-wonen.svg";
+import nedapLogo from "../images/clients/nedap.svg";
+import ponLogo from "../images/clients/pon.svg";
+import sequentialLogo from "../images/clients/sequential.png";
+import shotByModelsLogo from "../images/clients/shot-by-models.png";
+import sooqrLogo from "../images/clients/sooqr.png";
+import websightLogo from "../images/clients/websight.svg";
+import wfcLogo from "../images/clients/wfc.svg";
+import klmLogo from "../images/logos/klm.svg";
+import sanomaLogo from "../images/logos/sanoma.svg";
+import rtlLogo from "../images/logos/rtl.svg";
+
+export const EMPLOYEES = [
+  {
+    src: brandshakersLogo,
+    alt: "Brandshakers"
+  },
+  {
+    src: flyershotsLogo,
+    alt: "Flyershots"
+  },
+  {
+    src: shotByModelsLogo,
+    alt: "Shot by Models",
+    backgroundColor: "#000000"
+  },
+  {
+    src: websightLogo,
+    alt: "Websight"
+  },
+
+  {
+    src: sequentialLogo,
+    alt: "Sequential"
+  },
+  {
+    src: klmLogo,
+    alt: "KLM"
+  },
+  {
+    src: sanomaLogo,
+    alt: "Sanoma",
+    backgroundColor: "#000000"
+  },
+
+  {
+    src: rtlLogo,
+    alt: "RTL Nederland",
+    backgroundColor: "#000000"
+  }
+];
+
+export const CLIENTS = [
+  {
+    src: alphaplusLogo,
+    alt: "Alphaplus",
+    backgroundColor: "#e5e6e5"
+  },
+  {
+    src: cosmopolitanLogo,
+    alt: "cosmopolitan"
+  },
+  {
+    src: dhlLogo,
+    alt: "DHL",
+    backgroundColor: "#fecc00"
+  },
+  {
+    src: ebayLogo,
+    alt: "Ebay"
+  },
+  {
+    src: ebuddyLogo,
+    alt: "EBuddy"
+  },
+  {
+    src: flyershotsLogo,
+    alt: "Flyershots"
+  },
+  {
+    src: geekSquadLogo,
+    alt: "Geek Squad"
+  },
+  {
+    src: gemeenteHilversum,
+    alt: "Gemeente Hilversum"
+  },
+  {
+    src: hansTeeuwen,
+    alt: "Hans Teeuwen",
+    backgroundColor: "#000000",
+    cover: true
+  },
+  {
+    src: klantenBedrijfLogo,
+    alt: "Klantenbedrijf"
+  },
+  {
+    src: kvkLogo,
+    alt: "Kamer Van Koophandel"
+  },
+  {
+    src: mediamarktLogo,
+    alt: "Mediamarkt"
+  },
+  {
+    src: middelmanWonenLogo,
+    alt: "Middelman Wonen"
+  },
+  {
+    src: nedapLogo,
+    alt: "Nedap"
+  },
+  {
+    src: ponLogo,
+    alt: "PON"
+  },
+  {
+    src: shotByModelsLogo,
+    alt: "Shot by Models",
+    backgroundColor: "#000000"
+  },
+  {
+    src: sooqrLogo,
+    alt: "Sooqr"
+  },
+  {
+    src: websightLogo,
+    alt: "Websight"
+  },
+  {
+    src: wfcLogo,
+    alt: "World Fashion Center"
+  }
+];
