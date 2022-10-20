27ae54a0df9d74a99fca01b69caff49ebf42f68a
Raymond Schweers <raymond@hetwebbureau.nl>
2022-10-20 22:00:02 +0200

    refactor(uses) copy

---
+++ src/constants/uses.ts
@@ -0,0 +1,124 @@
+export const HARDWARE = {
+  title: "workstation",
+  items: [
+    {
+      title: "Macbook Air, M1",
+      description:
+        "Tried Windows, Ubuntu and MacOS, but I always come back to MacOS, what can i say? It's just a breeze + now I am one of the cool kids."
+    },
+    {
+      title: "49\" ultra wide screen",
+      description:
+        "Big, bigger, biggest, a big screen is indeed better, allthough screen sharing is a bit of a mess, but hey! This will resolve if everyone has an ultra wide screen."
+    },
+    {
+      title: "Custom WASD Mechanical keyboard",
+      description:
+        "The texture when I touch one of the PBT keys, the tactical feedback when I am pressing a key, the sound not to loud but just perfect, oh, yes the satisfaction!"
+    },
+    {
+      title: "Headphones",
+      description:
+        "My daily dose of lossless vitamines goes through some special instruments to enhance the recipe. It's starts going through an DAC, then an headphone amplifier which on it's turn end up in one of my specially collected headphones. This formula will make any developer headbanging healthy."
+    },
+  ]
+};
+
+export const DEVELOPMENT = {
+  title: "Development tools",
+  items: [
+    {
+      title: "LunarVim",
+      description:
+        "\"What editor do you have? Is it a special vscode theme?\" It's LunarVim, an IDE layer for Neovim. It's blazzzzing fassst. If you only knew how many undo's i am able to do in different files at once."
+    },
+    {
+      title: "tmux",
+      description:
+        "Black and white, yin and yang, vim and tmux, yes, pure joy, tmuxinator makes it even more joyfull. Just type a repository name and it will boot up everything.. yes, everything.. With just one command, a kind of magic!"
+    },
+    {
+      title: "lazygit",
+      description:
+        "Look cool as a hacker, like you are the superstar in one of those hacker hollywood movies, while you just simply commiting some changes. Jokes aside, it's a perfect fit with vim and tmux. You never need to leave the terminal again. All the things with keyboard only."
+    },
+    {
+      title: "nodejs",
+      description:
+        "What can i say as a Frontend Developer, kind of obvious to play with nodejs."
+    },
+    {
+      title: "docker",
+      description:
+        "I like big blue whales with a kindly face, especially when they are hyped up. I hope I ever will see one IRL and swim with one, that would be something."
+    },
+  ]
+};
+
+export const SERVICES = {
+  title: "Favorite online services",
+  items: [
+    {
+      title: "Github",
+      description:
+        "My favorite bar to visit to share some beers or to try something new for once."
+    },
+    {
+      title: "Firebase",
+      description:
+        "It's so easy, I feel like I can create the worlds most complex app."
+    },
+    {
+      title: "AWS",
+      description:
+        "It's so complex, but the powers that you get, oh boy."
+    },
+    {
+      title: "Harvest",
+      description:
+        ""
+    },
+  ]
+}
+
+export const DESIGN = {
+  title: "Design",
+  items: [
+    {
+      title: "Photoshop",
+      description:
+        "Guilty, i am one of those who back in the day created webdesigns in Photoshop. Now i am really good in Photoshopping friends."
+    },
+    {
+      title: "Figma",
+      description:
+        "lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+    },
+    {
+      title: "Balsamiq",
+      description:
+        "lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+    },
+  ]
+};
+
+export const PRODUCTIVITY = {
+  title: "Productivity ",
+  items: [
+    {
+      title: "yabai",
+      description:
+        "You will feel like a mad ninja when you use Yabai when going through the jungle of running applications."
+    },
+    {
+      title: "skhd",
+      description:
+        "hotkeys.. hotkeys... hotkeys.. we want hotkeys!"
+    },
+    {
+      title: "dotfiles",
+      description:
+        "I can trash my computer and not worry, because i can have it back in minutes via my custom dotfiles setup."
+    },
+  ]
+};
