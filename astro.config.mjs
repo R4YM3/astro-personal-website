f0f8d84cd634f14480247489d063c53767e27c12
Raymond Schweers <raymond@hetwebbureau.nl>
2022-10-31 21:50:36 +0100

    feat(astro) compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.raymondschweers.nl",
   base: "/",
-  integrations: [tailwind()]
+  integrations: [tailwind(), compress()]
 });
